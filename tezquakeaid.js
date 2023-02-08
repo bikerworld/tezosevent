94414f380bdce8c50fa20b7603ada011e151366e
NftBiker <87770528+nftbiker@users.noreply.github.com>
2023-02-08 12:50:40 +0100

    Create tezquakeaid.js

---
+++ tezquakeaid.js
@@ -0,0 +1,11 @@
+{
+  "title": "TezQuakeAid",
+  "description": "",
+  "tags": ["TezQuakeAid"],
+  "start_time": "2023-02-01T12:00:00Z",
+  "admins": [],
+  "extra": {
+    "url": "",
+    "version": "v3"
+  }
+}
